4383be644c59bad0675aa5501b83c1186f527fc4
ruzzy13 <uzma.zhafira@gmail.com>
2026-03-10 12:04:24 +0700

    [GREEN] add implement for payment repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,10 +11,24 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.set(i, payment);
+                return payment;
+            }
+            i++;
+        }
+        paymentData.add(payment);
         return payment;
     }
 
     public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
 
