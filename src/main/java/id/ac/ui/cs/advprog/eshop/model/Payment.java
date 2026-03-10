1b27a197ad60ec11fa080893487b30c53ef0ee66
ruzzy13 <uzma.zhafira@gmail.com>
2026-03-10 11:26:08 +0700

    [GREEN] implement model payment

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -15,5 +15,10 @@ public class Payment {
     private Order order;
 
     public Payment(String id, String method, Map<String, String> paymentData, Order order) {
+        this.id = id != null ? id : UUID.randomUUID().toString();
+        this.method = method;
+        this.paymentData = paymentData;
+        this.order = order;
+        this.status = "WAITING_PAYMENT";
     }
 }
