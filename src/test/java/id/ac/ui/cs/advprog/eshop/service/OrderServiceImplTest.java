b867186c98b50acf322245e3bf4d36c330e9b2dc
ruzzy13 <uzma.zhafira@gmail.com>
2026-03-10 09:38:26 +0700

    [GREEN] Implement OrderRepository class.

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -18,6 +18,7 @@ import static org.mockito.Mockito.*;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceImplTest {
