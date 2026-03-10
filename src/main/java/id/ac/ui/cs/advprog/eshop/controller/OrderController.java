103085e05ee4fa1aed4a570fc0a70e4c57fb0276
ruzzy13 <uzma.zhafira@gmail.com>
2026-03-10 13:51:38 +0700

    [GREEN] implements for order controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -18,25 +18,43 @@ public class OrderController {
 
     @GetMapping("/create")
     public String createOrderPage(Model model) {
+        return "order/createOrder";
     }
 
     @PostMapping("/create")
     public String createOrderPost(@ModelAttribute Order order) {
+        orderService.createOrder(order);
+        return "redirect:/order/history";
     }
 
     @GetMapping("/history")
     public String historyOrderPage() {
+        return "order/historyOrder";
     }
 
     @PostMapping("/history")
     public String historyOrderPost(@RequestParam String author, Model model) {
+        List<Order> orders = orderService.findAllByAuthor(author);
+        model.addAttribute("orders", orders);
+        return "order/historyOrder";
     }
 
     @GetMapping("/pay/{orderId}")
     public String paymentOrderPage(@PathVariable String orderId, Model model) {
+        Order order = orderService.findById(orderId);
+        model.addAttribute("order", order);
+        return "order/payOrder";
     }
 
     @PostMapping("/pay/{orderId}")
     public String paymentOrderPost(@PathVariable String orderId, Model model) {
+        Order order = orderService.findById(orderId);
+
+        String generatedPaymentId = "PAYMENT-" + orderId;
+
+        model.addAttribute("order", order);
+        model.addAttribute("paymentId", generatedPaymentId);
+
+        return "order/paymentResult";
     }
 }
